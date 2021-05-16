54651ee3ba09e74bc6fd590e6bfa93e6322ea795
Hongwei Mou <79674095+henrymouhw@users.noreply.github.com>
2021-05-17 01:16:27 +1000

    Delete build/web directory

--- build/web/db/Create_StaffRecord -Jessie.sql
+++
@@ -1,7 +0,0 @@
-create table STAFF (
-name varchar(300) not null, 
-email varchar(300) not null primary key,
-position varchar(300) not null,
-address varchar(800),
-status varchar(100) not null
- )
